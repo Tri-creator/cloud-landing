8d872ac21916b94269486e0110a8abd4f1e443e7
Tri-creator <ethanngo2k5@gmail.com>
2026-07-03 09:58:12 +0700

    feat: refactor document download links to use dynamic rendering and improve code readability

--- src/app/(frontend)/documents/page.tsx
+++ src/app/(frontend)/documents/page.tsx
@@ -5,25 +5,21 @@ const documents = [
     title: 'Hướng dẫn sử dụng Website VCloud',
     subtitle: 'Người dùng',
     image: '/clients/9.png',
-    file: 'https://drive.google.com/file/d/137Nm1hg43O-t3kueRHkgdlpYq2dwA8QL/view?usp=sharing',
   },
   {
     title: 'Hướng dẫn sử dụng Website VCloud Biển số xe',
     subtitle: 'Người dùng',
     image: '/clients/9.png',
-    file: 'https://drive.google.com/file/d/1kM2mQlv7hnaqgm7IoOhFZpvmFsp90Vip/view?usp=sharing',
   },
   {
     title: 'Hướng dẫn sử dụng ứng dụng di động VCloud',
     subtitle: 'Người dùng',
     image: '/clients/9.png',
-    file: 'https://drive.google.com/file/d/1pMUcJi0gE1TQ8kTemoM_MBSzeZ12Bk2h/view?usp=sharing',
   },
   {
     title: 'Hướng dẫn sử dụng Desktop App VCloud',
     subtitle: 'Người dùng',
     image: '/clients/9.png',
-    file: 'https://drive.google.com/file/d/1En2y26KrdDVS1aYZ9pdlFub7mZZju9Lm/view?usp=sharing',
   },
 ]
 
@@ -42,15 +38,49 @@ export default function DocumentsPage() {
 
               <h2>{doc.title}</h2>
               <p>– {doc.subtitle}</p>
+                {index === 0 && (
+  <a
+    className="document-download"
+    href="https://drive.google.com/file/d/137Nm1hg43O-t3kueRHkgdlpYq2dwA8QL/view?usp=sharing"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Tải xuống
+  </a>
+)}
 
-              <a
-                href={doc.file}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="document-download"
-              >
-                Tải xuống
-              </a>
+{index === 1 && (
+  <a
+    className="document-download"
+    href="https://drive.google.com/file/d/1kM2mQlv7hnaqgm7IoOhFZpvmFsp90Vip/view?usp=sharing"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Tải xuống
+  </a>
+)}
+
+{index === 2 && (
+  <a
+    className="document-download"
+    href="https://drive.google.com/file/d/1pMUcJi0gE1TQ8kTemoM_MBSzeZ12Bk2h/view?usp=sharing"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Tải xuống
+  </a>
+)}
+
+{index === 3 && (
+  <a
+    className="document-download"
+    href="https://drive.google.com/file/d/1En2y26KrdDVS1aYZ9pdlFub7mZZju9Lm/view?usp=sharing"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Tải xuống
+  </a>
+)}
             </div>
           ))}
         </div>
